8853c3c4e82151edc4d3327ca2d6d5b69f0a2b5c
gokayoz <gokay.oz@outlook.com.tr>
2025-04-28 19:36:14 +0300

    Adds Bolum class and properties

---
+++ HastaneSimulasyonu.Core/Models/Bolum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HastaneSimulasyonu.Core.Models
+{
+    public class Bolum
+    {
+        public int Id { get; set; }
+        public string Ad { get; set; }
+        public string Aciklama { get; set; }
+        public List<Doktor> Doktorlar { get; set; }
+    }
+}
