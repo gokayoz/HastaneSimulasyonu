be11ca49522e19beec3bf16b9802ce4c8e94ad19
gokayoz <gokay.oz@outlook.com.tr>
2025-04-29 18:11:48 +0300

    Makes add-migrations and updates the database

---
+++ HastaneSimulasyonu.Core/Migrations/20250429151006_m2.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HastaneSimulasyonu.Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class m2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DogumTarihi",
+                table: "Hasta");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Sikayet",
+                table: "Hasta",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Sikayet",
+                table: "Hasta");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DogumTarihi",
+                table: "Hasta",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+    }
+}
